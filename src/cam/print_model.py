c373b09f86ffc85f939fe33f7bfc6599ee8bb3b8
Elijah <57039829+jielin1997@users.noreply.github.com>
2022-12-02 20:39:30 +0800

    Delete src directory

--- src/cam/print_model.py
+++
@@ -1,3 +0,0 @@
-from efficientnet_pytorch import EfficientNet
-model = EfficientNet.from_pretrained('efficientnet-b4')
-print(model)
--- src/common/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Useful utilities unrelated to the ML pipeline."""
-
-# Import everything toplevel to reduce redundancy in imports.
-from .utils import *
-
-from .tensorlist import TensorList
